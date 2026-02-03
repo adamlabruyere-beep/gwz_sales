3fa1d6557c93e70d9dbc35b4a0f24fd575c240db
Adam Labruyere <adam.labruyere@gmail.com>
2026-02-03 12:01:19 +0100

    new

---
+++ gwz_sales.sql
@@ -0,0 +1,10 @@
+WITH ddq AS (
+SELECT date_date,
+SUM(qty) AS qty
+FROM `data-analytics-sql-485211.course15.circle_sales`
+GROUP BY date_date
+)
+
+SELECT
+ROUND(SUM(qty)/COUNT(date_date),0) AS day_q
+FROM ddq
