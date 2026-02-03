b23285893f520d8721873ad8696d634fbc6fa48e
Adam Labruyere <adam.labruyere@gmail.com>
2026-02-03 12:16:17 +0100

    purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,10 +1,12 @@
 WITH ddq AS (
 SELECT date_date,
+SUM(purchase_cost) AS purchase_cost
 SUM(qty) AS qty
 FROM `data-analytics-sql-485211.course15.circle_sales`
 GROUP BY date_date
 )
 
 SELECT
+purchase_cost
 ROUND(SUM(qty)/COUNT(date_date),0) AS day_q
 FROM ddq
